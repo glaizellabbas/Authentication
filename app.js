20624d40d835f9bb362c72a57df67f8b8cbd69a0
Glaizell <glaizellabbas@gmail.com>
2021-01-17 19:38:25 +0800

    Salting and Hashing with bcrypt

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
 
 const app = express();
 
@@ -39,18 +43,24 @@ app.get('/register', (req, res) => {
     });
 
 app.post('/register', (req, res) => {
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        // Store hash in your password DB.
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+    
+        newUser.save(function(err){
+            if(err){
+                console.log(err);
+            }else{
+                res.render("secrets")
+            }
+        });
     });
 
-    newUser.save(function(err){
-        if(err){
-            console.log(err);
-        }else{
-            res.render("secrets")
-        }
-    });
+   
 });
 
 app.post("/login", (req, res) => {
@@ -62,15 +72,19 @@ app.post("/login", (req, res) => {
           console.log(err);
       }else{
           if (foundUser) {
-              if (foundUser.password === password) {
-                  res.render("secrets")
+            bcrypt.compare( req.body.password, foundUser.password, function(err, result) {
+                if(result === true){
+                    res.render("secrets")
+                }
+            });
+                 
                   
               }
           }
-      }
-  });
-       
+  })
 });
+       
+
 
 
 
