40fd4adb9244b92eeb5039e53d1ef4925b89b9dc
Glaizell <glaizellabbas@gmail.com>
2021-01-17 14:11:23 +0800

    Register Username and Password

--- app.js
+++ app.js
@@ -2,12 +2,25 @@
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const { log } = require("console");
 
 const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded ({extended: true}));
+
+mongoose.connect("mongodb://localhost:27017/usersDB", {useNewUrlParser: true,  useUnifiedTopology: true });
+
+
+
+const userSChema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model('User', userSChema);
 
 app.get('/', (req, res) => {
     res.render("home")
@@ -20,6 +33,55 @@ app.get('/login', (req, res) => {
 app.get('/register', (req, res) => {
     res.render("register")
     });
+
+app.post('/register', (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save(function(err){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("secrets")
+        }
+    });
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email: username }, function(err, foundUser){
+      if(err){
+          console.log(err);
+      }else{
+          if (foundUser) {
+              if (foundUser.password === password) {
+                  res.render("secrets")
+                  
+              }
+          }
+      }
+  });
+       
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     app.listen(3000, () => {
